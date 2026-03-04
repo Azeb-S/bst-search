18f424d174867da61f49e42a4fb1022b98541d80
Azeb Tesfay <azbseb@gmail.com>
2026-03-04 15:38:54 -0800

    test_multipleLevels_itera

--- src/BstSearchTest.java
+++ src/BstSearchTest.java
@@ -44,4 +44,32 @@ public class BstSearchTest {
   assertTrue(BstSearch.contains(root, 100));
  }
 
+ @Test
+ void test_multipleLevels_itera() {
+  /*
+   * 13
+   * / \
+   * -5 42
+   * / \ /
+   * 9 9 100
+   * / \
+   * 31 7
+   *
+   * Leaf nodes: 9, 31, 7, 100
+   *
+   */
+  BinaryTreeNode<Integer> root = new BinaryTreeNode<>(13);
+  root.left = new BinaryTreeNode<>(-5);
+  root.right = new BinaryTreeNode<>(42);
+
+  root.left.left = new BinaryTreeNode<>(9);
+  root.left.right = new BinaryTreeNode<>(9);
+  root.left.right.left = new BinaryTreeNode<>(31);
+  root.left.right.right = new BinaryTreeNode<>(7);
+
+  root.right.left = new BinaryTreeNode<>(100);
+
+  assertTrue(BstSearch.containsIterativly(root, 100));
+ }
+
 }
