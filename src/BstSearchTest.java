dd16a8ce8c60c656bf33b692a48ae53c9fa8cc1b
Azeb Tesfay <azbseb@gmail.com>
2026-03-04 14:01:20 -0800

    write the contains and test for contains_emptyTree and contains_targe

--- src/BstSearchTest.java
+++ src/BstSearchTest.java
@@ -1,6 +1,19 @@
 import static org.junit.jupiter.api.Assertions.*;
+
+import javax.swing.tree.TreeNode;
+
 import org.junit.jupiter.api.Test;
 
 public class BstSearchTest {
+ @Test
+ void contains_emptyTree() {
+  assertFalse(BstSearch.contains(null, 10));
+ }
+
+ @Test 
+ void contains_target(){
+  BinaryTreeNode<Integer> root = new BinaryTreeNode<Integer>(47)
+  assertTrue(BstSearch.contains(root, 47));
+ }
 
 }
